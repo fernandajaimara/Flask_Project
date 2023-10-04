22e72ada3f8503e4a11f74a169b2aea7a0557c09
Fernanda Jaimara <fernandajaimara@gmail.com>
2023-10-03 21:51:27 -0300

    calculator add

--- primeiro_ambiente_py/um.py
+++ primeiro_ambiente_py/um.py
@@ -6,4 +6,19 @@ app = Flask(__name__)
 def raiz():
     return 'Olá Mundo!'
 
+@app.route('/calculadora/<int>:n1/<int>:n2/<string>:operador')
+def calculadora(n1, n2, operacao):
+    if operacao == "soma":
+        resultado = n1 + n2
+    elif operacao == "sub":
+        resultado = n1 - n2
+    elif operacao == "mult":
+        resultado = n1 * n2
+    elif operacao == "div":
+        resultado = n1 / n2
+    else:
+        resultado = "Operação não suportada"
+
+    return '{} {} {} = {}'.format(n1, operacao, n2, resultado)
+
 app.run()
